05c35fe42f44859d4ce8744b67e406064033abf7
Pavlo <pavlo.yamchinsky@eze.tech>
2024-09-29 22:19:13 +0300

    fix: fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import process from "node:process";
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/legenda-podillia/" : "/",
+  base: "/",
 });
