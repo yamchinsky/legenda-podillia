3658c99edeba57a7d32eb23b79cdaf30a9a74eea
Pavlo <pavlo.yamchinsky@eze.tech>
2024-10-06 11:41:44 +0300

    fix: path to repo

--- src/components/Reports.jsx
+++ src/components/Reports.jsx
@@ -69,9 +69,9 @@ export const Reports = () => {
                   const isLocal = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
                   const fileUrl = isLocal 
                     ? `/reports/${file.name}` 
-                    : `https://github.com/${owner}/${repo}/blob/main/${path}/${file.name}`;
+                    : `https://raw.githubusercontent.com/${owner}/${repo}/main/${path}/${file.name}`;
                   window.open(fileUrl, "_blank");
-                }}                
+                }}                               
               >
                 <Text
                   ellipsis={{ tooltip: file.name }}
